ca85571ed269f0176eb6f4a3d596a9730b8a6b0f
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-04-26 15:02:23 +0300

    variables val vs var

--- part_1/src/main/kotlin/main.kt
+++ part_1/src/main/kotlin/main.kt
@@ -1,4 +1,19 @@
 fun main(args: Array<String>) {
     val name = " Kotlin"
-    println("Hello $name")
+    println("Hello ${name}")  // String interpolations
+
+    // Variables
+
+    printVars()  
+
+}
+
+fun printVars() {
+    var age  = 20  // Mutable
+
+    val pi = 3.14 // Immutable
+
+    println(age)
+
+    println(pi)
 }
